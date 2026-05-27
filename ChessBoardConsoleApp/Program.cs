b28ae96b3ece44d156768245908d2f8c82f59423
IPFizzy <fizzy@keonbushman.com>
2026-05-27 11:12:20 -0700

    Add user input from chess pieces to the console app.

--- ChessBoardConsoleApp/Program.cs
+++ ChessBoardConsoleApp/Program.cs
@@ -7,14 +7,35 @@
  * Activity 2 Guide
  */
 
+using ChessBoardClassLibrary.Models;
+
 //---------------------------------------------------------------
 // Start of Main Method
 //---------------------------------------------------------------
 
-// Print a welcome message for the user
-using ChessBoardClassLibrary.Models;
+// Declare and initialize
+string piece = "";
+Tuple<int, int>? result;
 
+// Print a welcome message for the user
 Console.WriteLine("Hello, Chess Players!");
+
+// Create a new chess board
+BoardModel board = new BoardModel(8);
+
+// Show the empty board
+Utility.PrintBoard(board);
+
+// Prompt the user for the type of chess piece
+Console.Write("Enter the type of piece you want to place (Knight, Rook, Bishop, Queen, King): ");
+piece = Console.ReadLine();
+
+// Prompt the user for the location of the chess piece
+result = Utility.GetRowAndCol();
+
+// Mark the legal moves based on the input
+
+// Print out the new chess board
 
 //---------------------------------------------------------------
 // End of Main Method
@@ -65,4 +86,22 @@ public static class Utility
             Console.WriteLine();
         }
     } // End of PrintBoard method
+
+    /// <summary>
+    /// Get the row and column for the piece.
+    /// </summary>
+    /// <returns></returns>
+    internal static Tuple<int, int> GetRowAndCol()
+    {
+        // Get the row from the user
+        Console.Write("Enter the row number of the piece: ");
+        int row = int.Parse(Console.ReadLine());
+
+        // Get the column from the user
+        Console.Write("Enter the column number of the piece: ");
+        int col = int.Parse(Console.ReadLine());
+
+        // Return the data
+        return Tuple.Create(row, col);
+    }
 }
