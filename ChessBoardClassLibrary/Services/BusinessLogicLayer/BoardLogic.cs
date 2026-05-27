c59333c0f16b241707b64476a127845e3ba2320f
IPFizzy <fizzy@keonbushman.com>
2026-05-27 09:58:19 -0700

    Add logic to mark knight moves as valid.

--- ChessBoardClassLibrary/Services/BusinessLogicLayer/BoardLogic.cs
+++ ChessBoardClassLibrary/Services/BusinessLogicLayer/BoardLogic.cs
@@ -61,5 +61,78 @@ namespace ChessBoardClassLibrary.Services.BusinessLogicLayer
             // Return true if both row and column are safe
             return isRowSafe && isColumnSafe;
         }
+
+        /// <summary>
+        /// Mark the legal moves for the given piece and location.
+        /// </summary>
+        /// <param name="board"></param>
+        /// <param name="currentCell"></param>
+        /// <param name="chessPiece"></param>
+        /// <returns></returns>
+        public BoardModel MarkLegalMoves(BoardModel board, CellModel currentCell, string chessPiece)
+        {
+            // Reset the board
+            board = ResetBoard(board);
+
+            // Use a switch statement to determine the behavior of the piece
+            switch (chessPiece.ToLower())
+            {
+                case "knight":
+                    // Set the occupying property for the current cell
+                    board.Grid[currentCell.Row, currentCell.Column].PieceOccupyingCell = "N";
+                    // Mark the valid moves for the knight
+                    board = MarkValidKnightMoves(board, currentCell);
+                    break;
+
+                case "rook":
+                    break;
+
+                case "bishop":
+                    break;
+
+                case "queen":
+                    break;
+
+                case "king":
+                    break;
+
+                default:
+                    // If the piece is not valid, return the board as is
+                    return board;
+            }
+
+            // Return the updated board
+            return board;
+        } // End of MarkLegalMoves method
+
+        /// <summary>
+        /// Mark the valid moves for the knight.
+        /// Access modifier is private meaning this method is encapsulated within the
+        /// BoardLogic class and cannot be accessed directly outside the class.
+        /// </summary>
+        /// <param name="board"></param>
+        /// <param name="currentCell"></param>
+        /// <returns></returns>
+        private BoardModel MarkValidKnightMoves(BoardModel board, CellModel currentCell)
+        {
+            // Possible moves for knight row
+            int[] knightRowMoves = { 2, 1, -1, -2, -2, -1, 1, 2 };
+
+            // Possible moves for knight column
+            int[] knightColMoves = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+            // Loop through the knights moves
+            for (int i = 0; i < knightRowMoves.Length; i++)
+            {
+                // Check if each move is on the board
+                if (IsOnBoard(board, currentCell.Row + knightRowMoves[i], currentCell.Column + knightColMoves[i]))
+                {
+                    // If the cell is on the board, label it as a possible move for the knight
+                    board.Grid[currentCell.Row + knightRowMoves[i], currentCell.Column + knightColMoves[i]].IsLegalNextMove = true;
+                }
+            }
+
+            return board;
+        }
     }
 }
